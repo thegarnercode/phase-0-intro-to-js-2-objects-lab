6e653435961b862cff6d3f9258210574b522953d
Denisha Garner <thegarnercode@gmail.com>
2022-04-17 14:15:26 -0400

    commit tree fad99f43642d03e6b31150ef9c6e9fafafa9f569

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {name: "denisha"}
+
+    function updateEmployeeWithKeyAndValue(employee,key,value){
+        const employeeInfo = {...employee}
+        employeeInfo[key] = value
+        return employeeInfo
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    const employeeUpdate = {key,value}
+    employee [key] = value
+ return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employeeDelete = {...employee}
+    delete employeeDelete[key] // if key is in brackets its referring to a variable, if a dot is in front its referring to a property
+    return employeeDelete
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    
+    delete employee[key]
+    return employee
+}
+
